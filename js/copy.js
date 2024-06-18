8c3ff28c64e942d4e66e7ec815f99e30c6e6e454
Marat Sibagatullin <lapo.ez.yt@gmail.com>
2024-06-18 19:04:20 +0300

    Changed a bit

--- js/copy.js
+++ js/copy.js
@@ -1,13 +1,28 @@
 function copyDiscord() {
-    var dsc = "exteragram"
-    navigator.clipboard.writeText(dsc);
     Swal.fire({
-        position: 'center',
-        icon: 'info',
-        title: 'discord username @exteragram',
-        footer: 'Copied!',
-        showConfirmButton: false,
-      });
+        title: "Discord",
+        text: "Copy username or open in new tab?",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Copy",
+        cancelButtonText: "Open in new tab"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Copied!",
+            text: "Username @exteragram is copied.",
+            icon: "success"
+        });
+        var dsc = "exteragram"
+        navigator.clipboard.writeText(dsc);
+    } else if (
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+        window.open('https://discord.com/users/1097383143708827679','_blank')
+    }
+  });
 }
 
 function copyTinkoffCard() {
@@ -22,4 +37,13 @@ function copyTinkoffCard() {
       });
   }
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
   

3ecd8145d27fe935d8dfd53b2e1d4062e4404fae
Marat Sibagatullin <lapo.ez.yt@gmail.com>
2024-06-18 20:04:30 +0300

    very

---
+++ js/main.js
@@ -0,0 +1,33 @@
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function multiFuncs() {
+    console.log("Hello World!")
+    console.log("Very interesting, isn't it?")
+    console.log("-------------------------------")
+    var visited = getCookie('visited');
+    if (visited) {
+        console.log("Welcome back!")
+    } else {
+        setCookie('visited',true,1337);
+        console.log("Welcome!")
+    }
+}

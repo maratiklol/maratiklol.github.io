04cf87e5dff596da1a7e541e6b3f347feb5457c2
Marat Sibagatullin <lapo.ez.yt@gmail.com>
2024-08-03 06:50:53 +0300

    added some favicons, changed space between buttons...

---
+++ js/easterEggs.js
@@ -0,0 +1,10 @@
+function playWiggle() {
+    var audio = new Audio('./assets/sounds/выгу выгу выгу.mp3');
+    audio.play();
+    document.getElementById("name").textContent = "выгу выгу выгу";
+    console.log("выгу выгу выгу")
+    audio.addEventListener("ended", function(){
+        audio.currentTime = 0;
+        document.getElementById("name").textContent = "mrksbgg";
+   });
+}

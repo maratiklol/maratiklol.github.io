716c024a28728321be1c9b99dfe74343c584c281
Marat Sibagatullin <lapo.ez.yt@gmail.com>
2024-08-05 15:15:20 +0300

    checkin da titlez

--- js/dynamicTitle.js
+++ js/dynamicTitle.js
@@ -5,23 +5,26 @@ function sleep(ms) {
 async function dynamicTitle() {
     let index = 0;
     const url = "https://mrksbgg.is-a.dev/";
-
+    let titles;
+    
     if (window.location.href === url + "404") {
-        titles = ["где я?", "ты потерялся."];
-    } else if (window.location.href === url) { 
-        titles = ["mrksbgg", "mrksbgg >.<"];
-    } else { 
-        titles = ["mrksbgg", "mrksbgg >.<"];
+      titles = ["где я?", "ты потерялся."];
+    } else if (window.location.href === url) {
+      titles = ["mrksbgg", "mrksbgg >.<"];
+    } else {
+      titles = ["mrksbgg", "mrksbgg >.<"]; 
     }
   
     while (true) {
+      if (titles) {
         document.title = titles[index];
         await sleep(500);
-
+  
         index++;
-
+  
         if (index === titles.length) {
           index = 0;
         }
+      }
     }
-}
+  }

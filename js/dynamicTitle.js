45f0cf3e0af4733f5ba5f616224c5e7aba65607d
Marat Sibagatullin <lapo.ez.yt@gmail.com>
2024-08-05 14:57:28 +0300

    titles now in 1 function (fuck js), and i removed some shit from index html

--- js/dynamicTitle.js
+++ js/dynamicTitle.js
@@ -3,33 +3,24 @@ function sleep(ms) {
 }
 
 async function dynamicTitle() {
-    const titles = ["mrksbgg", "mrksbgg >.<"];
     let index = 0;
-
+    const url = "https://mrksbgg.is-a.dev";
+    let titles;
+  
     while (true) {
-        document.title = titles[index];
-        await sleep(500);
-
-        index++;
-        
-        if (index === titles.length) {
-            index = 0;
+        if (window.location.href === `${url}/404.html`) { 
+            titles = ["где я?", "ты потерялся."];
+        } else if (window.location.href === `${url}`) {
+            titles = ["mrksbgg", "mrksbgg >.<"];
         }
-    }
-}
 
-async function dynTitle404() {
-    const titles = ["где я?", "ты потерялся."];
-    let index = 0;
-
-    while (true) {
         document.title = titles[index];
         await sleep(500);
 
         index++;
-        
+
         if (index === titles.length) {
-            index = 0;
+          index = 0;
         }
     }
 }

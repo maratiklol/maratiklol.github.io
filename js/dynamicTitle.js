0fb7100bac990c4fe064d6e17468acd8a92f8747
Marat Sibagatullin <lapo.ez.yt@gmail.com>
2024-12-03 16:32:41 +0300

    xd

--- js/dynamicTitle.js
+++ js/dynamicTitle.js
@@ -1,30 +1,30 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function dynamicTitle() {
-    let index = 0;
-    const url = "https://mrksbgg.is-a.dev/";
-    let titles;
+  let index = 0;
+  const url = "https://mrksbgg.is-a.dev/";
+  let titles;
 
-    if (document.title === "ты потерялся.") {
-      titles = ["где я?", "ты потерялся."];
-    } else if (window.location.href === url) {
-      titles = ["mrksbgg", "mrksbgg >.<"];
-    } else {
-      titles = [document.title];
-    }
-  
-    while (true) {
-      if (titles) {
+  if (document.title === "ты потерялся.") {
+    titles = ["где я?", "ты потерялся."];
+  } else if (window.location.href === url) {
+    titles = ["mrksbgg", "mrksbgg >.<"];
+  } else {
+    titles = [document.title];
+  }
+
+  if (titles.length > 1) {
+      while (true) {
         document.title = titles[index];
         await sleep(500);
-  
+
         index++;
-  
+
         if (index === titles.length) {
           index = 0;
         }
       }
-    }
+  }
 }

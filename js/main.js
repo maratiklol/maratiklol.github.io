2a5001fd93e4c9f1523bc1c02672b28b5972f69e
Marat Sibagatullin <contact@mrksbgg.ru>
2026-02-17 16:28:10 +0300

    recode

--- js/main.js
+++ js/main.js
@@ -4,10 +4,32 @@ size = 48
 sizew = 48
 image.src = url;
 
-image.onload = function() {
+function clog(text) {
+    console.log("[xeon.lol] " + text);
+}
+
+asciiart = `           
+ ████                                             
+░░███                                             
+ ░███   ██████  █████ █████  ██████     █████ ████
+ ░███  ███░░███░░███ ░░███  ███░░███   ░░███ ░███ 
+ ░███ ░███ ░███ ░███  ░███ ░███████     ░███ ░███ 
+ ░███ ░███ ░███ ░░███ ███  ░███░░░      ░███ ░███ 
+ █████░░██████   ░░█████   ░░██████     ░░████████
+░░░░░  ░░░░░░     ░░░░░     ░░░░░░       ░░░░░░░░ 
+                                                  
+`
+
+image.onload = function () {
     var style = ['font-size: 1px;', 'padding: ' + this.height / 100 * size + 'px ' + this.width / 100 * sizew + 'px;', 'background: url(' + url + ') repeat;', //'background-size: contain;'
     ].join(' ');
     console.log('%c ', style);
+
+    console.log(asciiart);
+    clog("hello!");
+    clog("telegram: @steamhvh");
+    clog("this site is open-source btw");
+    clog("github.com/maratiklol/maratiklol.github.io");
 }
 
 // СЫРНЕНЬКО ᗜˬᗜ
@@ -36,3 +58,35 @@ image.onload = function() {
 // ⢸⡠⡀⠀⠒⠂⠐⠢⠀⣀⠀⠀⠀⠀⠀⢀⠤⠚⠀⠀⢸⣔⢄⠀⢾⠀
 // ⠀⠑⠸⢿⠀⠀⠀⠀⢈⡗⠭⣖⡒⠒⢊⣱⠀⠀⠀⠀⢨⠟⠂⠚⠋⠀
 // ⠀⠀⠀⠘⠦⣄⣀⣠⠞⠀⠀⠀⠈⠉⠉⠀⠳⠤⠤⡤⠞⠀⠀⠀⠀⠀
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cursorDot = document.querySelector(".cursor-dot");
+    const cursorOutline = document.querySelector(".cursor-outline");
+
+    if (window.matchMedia("(pointer: fine)").matches) {
+        window.addEventListener("mousemove", function (e) {
+            const posX = e.clientX;
+            const posY = e.clientY;
+
+            cursorDot.style.left = `${posX}px`;
+            cursorDot.style.top = `${posY}px`;
+
+            cursorOutline.animate({
+                left: `${posX}px`,
+                top: `${posY}px`
+            }, { duration: 500, fill: "forwards" });
+        });
+
+        const interactables = document.querySelectorAll("a, button, .social-item");
+
+        interactables.forEach(el => {
+            el.addEventListener("mouseenter", () => {
+                cursorOutline.classList.add("cursor-hover");
+            });
+            el.addEventListener("mouseleave", () => {
+                cursorOutline.classList.remove("cursor-hover");
+            });
+        });
+    }
+});
